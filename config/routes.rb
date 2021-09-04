223c74eb30a40552db05ec42878e9acc7c162186
JFlec <josephflec@gmail.com>
2021-09-04 17:34:29 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :destroy, :update, :index]
+    resources :videos, only: [:show, :index, :create, :destroy, :update]
+    resources :likes, only: [:create, :destroy, :update]
+    resources :dislikes, only: [:create, :destroy, :update]
+    resources :comments, only: [:create, :destroy, :update]
+    resource :session, only: [:create, :destroy]
+  end
 end
