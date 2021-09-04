5e56c275e609dbdda72adfdbdafd13fc81c77b2c
JFlec <josephflec@gmail.com>
2021-09-04 17:14:27 -0500

    modified controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,17 +47,31 @@ class User < ApplicationRecord
     end
 
     def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(p)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
     def reset_session_token
-        self.session_token = SecureRandom::urlsafe_base64(16)
+        generate_session_token
         self.save!
         self.session_token
     end
 
+    private
+
     def ensure_session_token
-        self.session_token ||= SecureRandom.urlsafe_base64(16)
+        generate_session_token unless self.session_token
+    end
+
+    def new_session_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def generate_session_token
+        self.session_token = new_session_token
+        while User.find_by(session_token: self.session_token)
+            self.session_token = new_session_token
+        end
+        self.session_token
     end
 
 end
