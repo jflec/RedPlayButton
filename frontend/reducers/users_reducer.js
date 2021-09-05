02342004d5a2e31176d80d35eacbc69a4f24f7f6
JFlec <josephflec@gmail.com>
2021-09-04 22:13:21 -0500

    auth continued

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,25 @@
+import {
+    RECEIVE_CURRENT_USER,
+} from '../actions/session_actions';
+
+import {
+    RECEIVE_USERS
+} from '../actions/user_actions';
+
+const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+    switch (action.type) {
+        case RECEIVE_USERS:
+            return Object.assign({}, state, ...action.users)
+
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, state, { [action.user.id]: action.user })
+
+        default:
+            return state;
+    }
+}
+
+export default usersReducer
+
