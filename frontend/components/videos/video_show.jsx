c60b120e5c59973dfe82399f23b73d0a9cc5280c
JFlec <josephflec@gmail.com>
2021-09-16 18:47:47 -0500

    styling for presentation

--- frontend/components/videos/video_show.jsx
+++ frontend/components/videos/video_show.jsx
@@ -4,6 +4,7 @@ import VideoPlayer from './video_player';
 import SideRowsContainer from './side_rows_container';
 import CommentFormContainer from '../comments/comment_form_container';
 import CommentIndexContainer from '../comments/comment_index_container';
+import { NavLink } from 'react-router-dom';
 
 function VideoShow(props) {
   const { videos, video, userId } = props;
@@ -36,14 +37,16 @@ function VideoShow(props) {
           <div className="video-details-container">
             <p className="video-title">{video.title}</p>
             <div className="video-channel-details">
-              {video.user.profile_picture_url ? (
-                <img
-                  className="user-icon-large"
-                  src={video.user.profile_picture_url}
-                ></img>
-              ) : (
-                <img className="user-icon-large" src={window.defaultPFP} />
-              )}
+              <NavLink exact to={`/c/${video.user.username}`}>
+                {video.user.profile_picture_url ? (
+                  <img
+                    className="user-icon-large"
+                    src={video.user.profile_picture_url}
+                  ></img>
+                ) : (
+                  <img className="user-icon-large" src={window.defaultPFP} />
+                )}
+              </NavLink>
               <p className="video-channel">{video.user.username}</p>
               <p className="video-description">{video.description}</p>
             </div>
